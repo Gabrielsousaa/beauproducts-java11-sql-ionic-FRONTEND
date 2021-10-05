d4567f292a4d1808f46849d2a2728a4a0ab278b2
Gabriel de Sousa Souto <44745576+Gabrielsousaa@users.noreply.github.com>
2021-10-05 13:08:14 -0300

    Tratamento padrao para outros erros com alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,11 +2,13 @@ import { Injectable } from "@angular/core";
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable } from "rxjs/Rx";
 import { StorageService } from "../services/storage.service";
+import { AlertController } from "ionic-angular";
+
 
 @Injectable()
 export class errorInterceptor implements HttpInterceptor {
 
-  constructor(public storage: StorageService){
+  constructor(public storage: StorageService, public alertCtrl: AlertController) {
 
   }
 
@@ -28,8 +30,10 @@ export class errorInterceptor implements HttpInterceptor {
           case 403:
             this.handle403();
             break;
-
+          case 401:
+            this.handle401();
           default:
+            this.handleDefaultError(errorObj);
             break;
         }
 
@@ -39,6 +43,24 @@ export class errorInterceptor implements HttpInterceptor {
 
   handle403() {
     this.storage.setLocalUser(null);
+  }
+  handle401() {
+    let alert = this.alertCtrl.create({
+      title: 'Falha de autenticação',
+      message: 'Email ou senha incorretas',
+      enableBackdropDismiss: false,
+      buttons: [{ text: 'Ok' }]
+    });
+    alert.present();
+  }
+  handleDefaultError(errorObj) {
+    let alert = this.alertCtrl.create({
+      title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+      message: errorObj.error,
+      enableBackdropDismiss: false,
+      buttons: [{ text: 'Ok' }]
+    });
+    alert.present();
   }
 
 
