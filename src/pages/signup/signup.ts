aa8e05c1e0007f3935344722acb157e95a9755b7
Gabriel de Sousa Souto <44745576+Gabrielsousaa@users.noreply.github.com>
2021-10-06 12:12:36 -0300

    criando formgroup para controlar o formulario

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the SignupPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,10 +9,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  formGroup: FormGroup;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
+    this.formGroup = formBuilder.group({
+      name: ['Joagquin', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
+      email: ['joaguin@gmail.com', [Validators.required, Validators.email]],
+      tipo: ['1', [Validators.required]],
+      cpfOuCnpj: ['05645767865', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
+      senha: ['123', [Validators.required]],
+      logradouro: ['Rua', [Validators.required]],
+      endereco: ['Av tomé de souza', [Validators.required]],
+      numero: ['4', [Validators.required]],
+      bairro: ['Jardim vila galvão', [Validators.required]],
+      cep: ['07054021', [Validators.required]],
+      telefone: ['11982523608', [Validators.required]],
+      estateId: ['2', [Validators.required]],
+      cityId: ['2', [Validators.required]],
+    });
   }
 
-  signupUser(){
+  signupUser() {
     console.log("Enviou o form");
   }
 
