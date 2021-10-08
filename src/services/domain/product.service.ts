05fdaf10eda173be631c818ee36aa1d2926da31a
Gabriel de Sousa Souto <44745576+Gabrielsousaa@users.noreply.github.com>
2021-10-08 12:19:21 -0300

    Carregando os produtos de uma dada categoria

---
+++ src/services/domain/product.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { API_CONFIG } from '../../config/api.config'
+import { Observable } from 'rxjs/Rx';
+
+
+@Injectable()
+export class ProductService{
+
+  constructor(public http: HttpClient){
+
+  }
+
+  findByCategory(category_id : string){
+    return this.http.get(`${API_CONFIG.baseUrl}/products/?categories=${category_id}`);
+  }
+
+
+}
